a1f619295b3c769de2b664aac95c40ff6f2a3139
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2016-01-22 13:01:22 +0500

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+  gulp.src('./*.js')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./*.html'], ['html']);
+  gulp.watch(['./*.js'], ['js']);
+});
+
+gulp.task('default', ['connect', 'watch']);
