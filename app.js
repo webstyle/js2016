650bda5625a598cba9952f223b8f3bfa196b7fb9
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2016-01-24 10:10:51 +0500

    first presentation

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
-var Man = function(user) {
-  this.name = user.name;
-  this.canCoding = true;
-  this.createdAt = new Date();
+// var simple = function(a, b) {
+//   return a + b;
+// };
+//
+// console.log(simple(23,23));
+
+var myObject = {
+  summa: function (a, b) {
+   console.log(this);
+   return a + b;
+  }
 };
 
-var farrux = new Man({
-  name: "Farrux"
-});
+// console.log(myObject.summa(23,34));
 
-console.log(farrux);
+var myObj = (function() {
+
+  function privateMethod() {
+    console.log(this);
+    return "Private Method";
+  }
+  return {
+    summa: function(a, b) {
+      var that = this;
+      function helper(a, b) {
+        console.log(that);
+        that.multiply = a * b;
+      }
+
+      helper(a,b);
+
+      return a +b;
+    },
+    publicMethod: privateMethod
+  };
+
+})();
+
+// console.log(myObj.summa(23,23));
+// console.log(myObj.publicMethod());
+// console.log(myObj.multiply);
+var arr = [23,23];
+var summa = function(a, b) {
+  console.log(this.summa(a,b));
+  console.log(this.multiply);
+  return a + b;
+};
+
+var add = summa.apply(myObj, arr);
+console.log(add);
