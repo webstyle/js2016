81dae6b06dd33a0ff9701e5b944c700a36e19445
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2016-02-14 10:44:45 +0500

    added simple animation

--- app.js
+++ app.js
@@ -6,24 +6,66 @@
 })();
 
 var ele = document.querySelector('#animation');
+ele.height = window.innerHeight;
+ele.width = window.innerWidth;
 var ctx = ele.getContext('2d');
 var x = 10;
 var y = 10;
-var width = 150;
-var height = 150;
+var duration = 0;
+var width = 50;
+var height = 50;
+var heading_x = Math.random() * 360;
+var heading_y = Math.random() * 360;
+var distance_x = 0;
+var distance_y = 0;
+
+ele.addEventListener('mousemove', function(evt) {
+    startX = x;
+    endX = evt.clientX;
+});
+
+function degressToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+function dir_x(length, angle) {
+    return length * Math.cos(degressToRadians(angle));
+}
+
+function dir_y(length, angle) {
+    return length * Math.sin(degressToRadians(angle));
+}
+
+function lerp(start, end, speed) {
+    return start + (end - start) * speed;
+}
 
 function logic() {
-    x += 10;
-    if (x < ele.width - width) {
-        requestAnimationFrame(draw);
+    if (heading_x > 360 || heading_x < -360) heading_x = 0;
+    if (heading_y > 360 || heading_y < -360) heading_y = 0;
+
+    if (x <= 0 || x >= ele.width - width) {
+        heading_x = heading_x + 180;
     }
+    if (y <= 0 || y >= ele.height - height) {
+        heading_y = -heading_y;
+    }
+
+    distance_x = dir_x(2, heading_x);
+    distance_y = dir_y(2, heading_y);
+
+    if (duration < 10) duration += 0.05;
+    x = lerp(x, x + distance_x, duration);
+    y = lerp(y, y + distance_y, duration);
+    requestAnimationFrame(draw);
+
 }
 
 function draw() {
     ctx.clearRect(0, 0, ele.width, ele.height);
     ctx.fillStyle = "#dadada";
-    ctx.fillRect(x, y, 150, 150);
+    ctx.fillRect(x, y, 50, 50);
 }
 
 requestAnimationFrame(draw);
-setInterval(logic, 100 / 6);
+setInterval(logic, 1000 / 6);
